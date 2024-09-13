9c4f43e547343a5942e5ef7197242be8045b9ee9
Difroy <unifroy@gmail.com>
2024-09-13 15:14:00 +0200

    Create TestRelazione.java

---
+++ com/generation/speedball/mode/entities/TestRelazione.java
@@ -0,0 +1,17 @@
+package com.generation.speedball.mode.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRelazione {
+
+	@Test
+	void test() {
+
+		Team t = new Team();
+		t.id = 1;
+
+	}
+
+}
