40054704f799bf234fdca0bcf4000c9d92be7378
Difroy <unifroy@gmail.com>
2024-09-13 15:26:45 +0200

    Update TestRelazione.java

--- com/generation/speedball/mode/entities/TestRelazione.java
+++ com/generation/speedball/mode/entities/TestRelazione.java
@@ -9,10 +9,34 @@ class TestRelazione {
 	@Test
 	void test() {
 
+		
 		Team t = new Team();
 		t.id = 1;
 		t.name = "Brutal Deluxe";
 
+		Player p1 = new Player();
+		p1.id = 1;
+		p1.name = "Ferdinando";
+		p1.role = Role.DEF;
+		
+		Player p2 = new Player();
+		p1.id = 2;
+		p1.name = "Maria Chiara";
+		p1.role = Role.ATK;
+		// t 		  is_a Team
+		// t.players  is_a List<Player> 
+		
+		// t has players
+		
+		// players is_a List<Player>
+		
+		//t.players.get(0)
+		
+		
+		t.players.add(p1);  
+		
+		t.players.add(p2);
 	}
+	
 
 }
