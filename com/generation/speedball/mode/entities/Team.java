9d9c0259de9590075c0f2466573786e0ca779176
Difroy <unifroy@gmail.com>
2024-09-13 15:11:13 +0200

    Update Team.java

--- com/generation/speedball/mode/entities/Team.java
+++ com/generation/speedball/mode/entities/Team.java
@@ -1,5 +1,18 @@
 package com.generation.speedball.mode.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
 
+	int id;
+	String name;  //BRUTAL DELUXE
+	//Lista dei miei player, dei miei lato n
+	//questo è un collegamente dal lato 1 al lato n
+	List<Player>players = new ArrayList<Player>();
+	
 }
+
+
+
+
