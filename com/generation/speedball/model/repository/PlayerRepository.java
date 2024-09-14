9a74e149cfad7fb201fdf02448cf932c96022d6a
Difroy <unifroy@gmail.com>
2024-09-14 16:26:24 +0200

    Update

--- com/generation/speedball/model/repository/PlayerRepository.java
+++ com/generation/speedball/model/repository/PlayerRepository.java
@@ -1,12 +1,23 @@
 package com.generation.speedball.model.repository;
 
+import java.util.List;
+
 import com.generation.speedball.model.entities.Player;
+import com.generation.speedball.model.entities.Team;
 
 public interface PlayerRepository {
 
 	
 	void insert (Player player);
 	Player findById(int id);
+	List<Player>findByTeam (Team team);
+	void update (Player player);
+	void delete (int id);
+	
+	
+	
+	
+	/*Non avendo bisogno di un ritorno dal database, uso solamente il tipo di ritorno void*/
 	
 	
 }
