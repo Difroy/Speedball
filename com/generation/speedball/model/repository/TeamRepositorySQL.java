5edbcf06b03793c4002f804ebce5044c8ecc6714
eduardinho99 <eduardop99@live.it>
2024-09-14 19:51:25 +0200

    update

--- com/generation/speedball/model/repository/TeamRepositorySQL.java
+++ com/generation/speedball/model/repository/TeamRepositorySQL.java
@@ -2,6 +2,8 @@ package com.generation.speedball.model.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.generation.speedball.model.entities.Team;
 
@@ -10,34 +12,75 @@ public class TeamRepositorySQL implements TeamRepository {
 	
 	Connection connessioneAlDatabase;
 
-	
+	PlayerRepository playerRepo = PlayerRepositoryFactory.make();
 	
 	@Override
-	public void insert(Team team) {
+	public void insert(Team team) 
+	{
 	
+		String sql = "insert into team (id, name, budget) values (?, ?, ?)";
 		
-		 PreparedStatement preparazioneSQL = connessioneAlDatabase(sql);
-		 
+		//alt shift y per finestra piccola
+		//ctrl shift f per formattare il codice
+		//ctrl shift o per importare
 		
-		
-		
+		 try 
+		 {
+			PreparedStatement  preparazioneSQL = connessioneAlDatabase.prepareStatement(sql);
+			preparazioneSQL.setInt(1, team.getId());
+			preparazioneSQL.setString(2, team.getName());
+			preparazioneSQL.setInt(3, team.getBudget());
+			
+			preparazioneSQL.execute();
+			
+		} catch 
+		 (SQLException e) 
+		 {
+			throw new RuntimeException(e.getMessage());
+		}
 	}
 
 	@Override
-	public Team findById(int id) {
-		// TODO Auto-generated method stub
+	public Team findById(int id) 
+	{
+		String sql = "select * from team where id = ?";
+		
+		
+		try 
+		{
+			PreparedStatement ps = connessioneAlDatabase.prepareStatement(sql);
+			ps.setInt(1, id);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			Team t = new Team();
+			if(rs.next())
+			{
+				t.setId(rs.getInt("id"));
+				t.setName(rs.getString("name"));
+				t.setBudget(rs.getInt("budget"));
+				
+				t.setPlayers(playerRepo.findByTeam(t));
+				
+			}
+		} 
+		catch (SQLException e) 
+		{
+			throw new RuntimeException(e.getMessage());
+		}
+		
 		return null;
 	}
 
 	@Override
-	public void update(Team team) {
-		// TODO Auto-generated method stub
+	public void update(Team team) 
+	{
 		
 	}
 
 	@Override
-	public void delete(int id) {
-		// TODO Auto-generated method stub
+	public void delete(int id) 
+	{
 		
 	}
 	
