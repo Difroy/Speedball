31e6df2093f98e5205646374a1bf9c3fee3ff6d1
Difroy <unifroy@gmail.com>
2024-09-14 12:39:18 +0200

    Update Team.java

--- com/generation/speedball/model/entities/Team.java
+++ com/generation/speedball/model/entities/Team.java
@@ -7,9 +7,46 @@ public class Team {
 
 	int id;
 	String name;  //BRUTAL DELUXE
+	int budget;
+	
 	//Lista dei miei player, dei miei lato n
 	//questo è un collegamente dal lato 1 al lato n
 	List<Player>players = new ArrayList<Player>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBudget() {
+		return budget;
+	}
+
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+	
+	
+	
 	
 }
 
